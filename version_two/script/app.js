dede51c9ab07cfa6864ca05b7331a7477d6953a9
jcapillo <jcapillo@directv.com>
2016-03-08 12:08:00 -0800

    increase the limit from 500 channels to show to 10000

--- version_two/script/app.js
+++ version_two/script/app.js
@@ -1,4 +1,4 @@
-var max_limit = 500, //the maximum limit for ng-repeat to display
+var max_limit = 10000, //the maximum limit for ng-repeat to display
     min_limit = 16,  //the minimum limit for ng-repeat to display
     localhost = true, //change this to false on production/staging
     app = angular.module('App', ['angular-growl','ngSanitize']);
